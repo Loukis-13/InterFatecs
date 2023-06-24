688558ddab7bf36ff57ff64a2ae8fbf9a3a23662
Loukis-13 <jose.hypolito@protonmail.com>
2023-06-24 00:23:55 -0300

    resolvido blaittland

--- InterFatecs2023_Fase1/blaittland/blaittland.py
+++ InterFatecs2023_Fase1/blaittland/blaittland.py
@@ -1,18 +1,46 @@
 input()
-books = input()
-ord_books = sorted(books)
+books = [*input()]
+sorted_books = sorted(books)
 
 read = 0
-for i in range(len(books)-1):
-    if ord(books[i]) > ord(books[i+1]):
-        x = 0
-        for j in range(i+1, len(books)):
-            if ord(books[i]) > ord(books[j]):
-                x += 1
-            else:
-                break
-        if x > 5:
+for i, b in enumerate(books):
+    s_book = sorted_books.index(b)
+
+    if s_book > i:
+        if s_book - i > 5:
             print("A Database Systems student read a book.")
-            exit(0)
-        read += x
-print(read)
+            break
+
+        read += s_book - i
+        sorted_books = sorted_books[:i] + [b] + sorted_books[i:s_book] + sorted_books[s_book+1:]
+else:
+    print(read)
+
+
+# primeira solução
+# verifica se a diferença entre algum dos indices dos livros ordenados
+# e desordenados é maior que cinco, caso seja imprime e encerra o programa.
+# Conta quantas ordenações do método Bubble Sort são necessárias para
+# ordenar os livros.
+
+# input()
+# books = [*input()]
+
+# sorted_books = sorted(books)
+# for i, b in enumerate(books):
+#     if sorted_books.index(b) - i > 5:
+#         print("A Database Systems student read a book.")
+#         exit(0)
+
+# read = 0
+# f = True
+# while f:
+#     f = False
+
+#     for i in range(len(books)-1):
+#         if ord(books[i]) > ord(books[i+1]):
+#             books[i], books[i+1] = books[i+1], books[i]
+#             read += 1
+#             f = True
+
+# print(read)
